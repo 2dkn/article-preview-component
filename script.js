6534f5b9568272b3bcdbb0225edce80ba7472556
2dkn <2dkneely@gmail.com>
2023-11-02 16:31:46 -0400

    fixed. next media queries then done

--- script.js
+++ script.js
@@ -3,16 +3,24 @@
 const sharebutton = document.querySelector(".article__button");
 const socials = document.querySelector(".socials");
 
-sharebutton.addEventListener("click", function () {
+sharebutton.addEventListener("click", function (e) {
+  e.stopPropagation();
   socials.style.display = "flex";
-  socials.style.animation = "fadeIn .3s ease-in-out";
+  socials.style.animation = "fadein .1s ease-in-out";
   socials.style.opacity = 1;
 
   socials.addEventListener("mouseleave", function () {
     socials.style.display = "none";
+    socials.style.animation = "fadeout .1s ease-in-out";
   });
 
   socials.addEventListener("animationend", function () {
     socials.style.animation = "";
+  });
+  document.addEventListener("click", function (e) {
+    if (!socials.contains(e.target)) {
+      // socials.style.animation = "fadeout .1s ease-in-out";
+      socials.style.display = "none";
+    }
   });
 });
