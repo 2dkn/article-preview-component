add282d7577baef6d5c4b853268fd098c88af7d4
2dkn <2dkneely@gmail.com>
2023-11-02 13:25:41 -0400

    Trying to get Javascript to work with animation and media queries

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 "use strict";
 
 const sharebutton = document.querySelector(".article__button");
+const socials = document.querySelector(".socials");
 
-sharebutton.addEventListener("click", function () {});
+sharebutton.addEventListener("click", function () {
+  socials.style.display = "flex";
+  socials.style.animation = "fadeIn .3s ease-in-out";
+  socials.style.opacity = 1;
+
+  socials.addEventListener("mouseleave", function () {
+    socials.style.display = "none";
+  });
+
+  socials.addEventListener("animationend", function () {
+    socials.style.animation = "";
+  });
+});
